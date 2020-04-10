831bdd9c86314f1c01e04c8671957c2cb8e789c5
Alina Lukina <alina.lukina.bfu@gmail.com>
2020-04-10 23:41:44 +0200

    slider in js

---
+++ app.js
@@ -0,0 +1,31 @@
+$(function (){
+
+/* Slider */
+    $("[data-slider]").slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      adaptiveHeight: true,
+      slidesToScroll: 1
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
